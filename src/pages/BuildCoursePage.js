41c70d9e239b6d979f2414520a37d9a74554e96c
Luke Harwood <lukeharwood11@gmail.com>
2022-10-11 20:09:33 -0500

    added sidebar

---
+++ src/pages/BuildCoursePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {CourseBuilder} from "../components/CourseBuilder";
+
+
+const BuildCourse = (props) => {
+    return (
+        <>
+            <SideBar/>
+            <CourseBuilder/>
+        </>
+    );
+}
