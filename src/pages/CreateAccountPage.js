ed9c754332ccc348736c985a1980357af66d4ff8
Luke Harwood <lukeharwood11@gmail.com>
2022-10-18 20:29:25 -0500

    intermittent commit

--- src/pages/CreateAccountPage.js
+++ src/pages/CreateAccountPage.js
@@ -7,16 +7,16 @@ const SignInPage = (props) => {
             initial={{ opacity: 0, scale: 0, y: "-1vh" }} animate={{ opacity: 1, scale: 1, y:0 }}  transition={{ ease: "anticipate", duration: .5}} exit={{opacity: 0, scale: 0}}
             className={"dialog w-full h-full bg-gray-100"}>
             <div className={"drop-shadow-lg rounded-lg flex justify-center flex-col w-1/3 mt-5 bg-white self-start"}>
-                <div><h1 className={"rounded-lg mb-2 p-5 text-white text-3xl bg-blue-500"}>Create Account</h1></div>
+                <div><h1 className={"border-b-2 border-gray-100 mb-2 p-2 text-gray-600 text-xl bg-gray-100"}>Create Account</h1></div>
                 <input type={"email"} className={"p-3 text-2xl m-2"} placeholder={"Email"}/>
                 <input className={"p-3 text-2xl m-2"} placeholder={"Password"} type={"password"}/>
                 <input className={"p-3 text-2xl m-2"} placeholder={"Confirm Password"} type={"password"}/>
                 <div className={"flex items-end"}>
-                    <motion.button whileHover={{scale: 1.05, x: 10}}
-                                   className={"w-full rounded-r-full bg-blue-500 text-white p-2"}>Create Account
+                    <motion.button whileTap={{ scale: .9}}
+                                   className={"w-full bg-blue-500 text-white p-2"}>Create Account
                     </motion.button>
                 </div>
-                <p className={"text-right p-2"}>Already have an account? <a className={"text-blue-500 underline"}
+                <p className={"text-right text-gray-600 p-2"}>Already have an account? <a className={"text-blue-500 underline"}
                                                                           href={"/sign-in"}>Sign in!</a></p>
             </div>
         </motion.div>
--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -12,13 +12,13 @@ const SignInPage = (props) => {
             initial={{ opacity: 0, scale: 0, y: "-1vh" }} animate={{ opacity: 1, scale: 1, y:0 }}  transition={{ ease: "anticipate", duration: .5}} exit={{opacity: 0, scale: 0}}
             className={"dialog w-full h-full bg-gray-100"}>
             <div className={"drop-shadow-lg rounded-lg flex justify-center flex-col w-1/3 mt-5 bg-white self-start"}>
-                <div><h1 className={"rounded-lg mb-2 p-5 text-white text-3xl bg-blue-500"}>Sign In</h1></div>
+                <div><h1 className={"border-b-2 border-gray-100 mb-2 p-2 text-gray-600 text-xl bg-gray-100"}>Sign In</h1></div>
                 <input type={"email"} className={"p-3 text-2xl m-2"} placeholder={"Email"}/>
                 <input className={"p-3 text-2xl m-2"} placeholder={"Password"} type={"password"}/>
                 <div className={"flex items-end"}>
-                    <motion.button onClick={handleSignIn} whileHover={{scale: 1.05, x: 10}} className={"w-full rounded-r-full bg-blue-500 text-white p-2"}>Log In</motion.button>
+                    <motion.button whileTap={{ scale: .9 }} onClick={handleSignIn} className={"w-full bg-blue-500 text-white p-2"}>Log In</motion.button>
                 </div>
-                <p className={"text-right p-2"}>Don't have an account? <a className={"text-blue-500 underline"} href={"/create-account"}>Create one!</a></p>
+                <p className={"text-right text-gray-600 p-2"}>Don't have an account? <a className={"text-blue-500 underline"} href={"/create-account"}>Create one!</a></p>
             </div>
         </motion.div>
     );
